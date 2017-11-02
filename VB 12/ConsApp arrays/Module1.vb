ba5480e4d5663549ecda80748124dca35bf8a0e5
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:39:52 -0400

    First commit

---
+++ VB 12/ConsApp arrays/Module1.vb
@@ -0,0 +1,25 @@
+﻿Module Module1
+
+    Sub Main()
+
+        'Arrays
+
+        Dim plate(3) As Integer
+
+        plate(0) = 100 'lower bound (zero based)
+        plate(1) = 200
+        plate(2) = 300
+        plate(3) = 400 'upperbound
+        plate(4) = 500 'one that doesnt exist (like having an imaginary freind at dinner table. theyre realy cant eat.)
+        Dim index As Integer
+
+        For index = LBound(plate) To UBound(plate)
+            Console.WriteLine("plate" & index & "is" & plate(index))
+
+        Next
+        Console.Read()
+
+      
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual Basic 12 -  Arrays
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=YWE90kNegpo
+
+Description: Learn about arrays and why we use them
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
